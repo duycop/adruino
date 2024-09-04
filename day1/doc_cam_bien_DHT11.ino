6a18f4bb0cea4c6ee7da5eefc4bb3d723b413907
duycop <duycop@tnut.edu.vn>
2024-09-04 22:46:15 +0700

    Update doc_cam_bien_DHT11.ino

--- day1/doc_cam_bien_DHT11.ino
+++ day1/doc_cam_bien_DHT11.ino
@@ -2,24 +2,25 @@
 #include <DHT.h>
 
 // Cấu hình DHT
-#define DHTPIN D4         //chan esp8266 ket noi cam bien DHT11
-#define DHTTYPE DHT11     //loai cam bien
-DHT dht(DHTPIN, DHTTYPE); //khai bao doi tuong dht thuoc class DHT , truyen vao 2 tham so: chan nao, loai sensor nao
+#define DHTPIN D4         //chân esp8266 kết nối cảm bien DHT11
+#define DHTTYPE DHT11     //loại cảm biến
+DHT dht(DHTPIN, DHTTYPE); //khai báo đối tượng dht thuộc class DHT , truyền vào 2 tham số: chân nào, loại sensor nào
 
 //run one time khi cấp điện
 void setup() {
-  dht.begin();                                //khởi tạo dht : vô số lệnh trong thư viện đã làm việc với cảm biến
-  Serial.begin(9600);
+  dht.begin();            //khởi tạo dht : vô số lệnh trong thư viện đã làm việc với cảm biến
+  Serial.begin(9600);     //khởi tạo truyền thông nối tiếp để debug
 }
 
-//hàm này mỗi khi chạy xong, hoặc đang chạy mà return: thì hàm này tự động được chạy lại: chạy mãi mãi mà ko cần có chỗ nào có lời gọi hàm này.
+//hàm này mỗi khi chạy xong, hoặc đang chạy mà return: 
+//thì hàm này tự động được chạy lại: chạy mãi mãi mà ko cần có chỗ nào có lời gọi hàm này.
 void loop() {
-  int h = dht.readHumidity();     //đọc độ ẩm
-  int t = dht.readTemperature();  //đọc nhiệt độ
-  Serial.print("DHT11: Do am = ");
-  Serial.print(h);
-  Serial.print("% Nhiet do = ");
-  Serial.print(t);  
-  Serial.println("°C");
-  delay(2000); // Đọc dữ liệu mỗi 2 giây, sau 2s hàm loop tự động đc gọi lại
+  int h = dht.readHumidity();     // đọc độ ẩm
+  int t = dht.readTemperature();  // đọc nhiệt độ
+  Serial.print("DHT11: Do am = ");// debug h
+  Serial.print(h);                // show h to uart
+  Serial.print("% Nhiet do = ");  // debug t
+  Serial.print(t);                // show t to uart
+  Serial.println("°C");           // xuất xong dữ liệu thì xuống dòng
+  delay(2000);                    // Đọc dữ liệu mỗi 2 giây, sau 2s hàm loop tự động đc gọi lại
 }
